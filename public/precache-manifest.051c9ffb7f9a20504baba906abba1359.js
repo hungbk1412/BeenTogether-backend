39c6cf0c2bdfbcab808e1012f1b21221e753949a
hungbk142180 <hung.nt142180@sis.hust.edu.vn>
2019-03-28 10:20:52 +0700

    Commit

---
+++ public/precache-manifest.051c9ffb7f9a20504baba906abba1359.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dd57ac5709ac5d5fa03598eb7c93119",
+    "url": "/static/media/bg.6dd57ac5.jpg"
+  },
+  {
+    "revision": "f7637757b2025ea38354dadf142af451",
+    "url": "/static/media/bannerForm.f7637757.jpg"
+  },
+  {
+    "revision": "1664fff485408ef7ece9e82224baa5cc",
+    "url": "/static/media/avatarDefault.1664fff4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4461bd0e3ff2b8e7387b",
+    "url": "/static/js/main.4461bd0e.chunk.js"
+  },
+  {
+    "revision": "dc3fa648cb473dd4f30b",
+    "url": "/static/js/1.dc3fa648.chunk.js"
+  },
+  {
+    "revision": "4461bd0e3ff2b8e7387b",
+    "url": "/static/css/main.f051962e.chunk.css"
+  },
+  {
+    "revision": "dc3fa648cb473dd4f30b",
+    "url": "/static/css/1.810b478b.chunk.css"
+  },
+  {
+    "revision": "8c83eaeb7b33507d6248a2d87c7c5238",
+    "url": "/index.html"
+  }
+];
